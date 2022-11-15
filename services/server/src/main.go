d7d191d3f5bde33a0f6ad6291eec0861f60a4789
ayumu-1212 <ayumuabe1434@gmail.com>
2022-11-16 04:13:26 +0900

    :construction: mysqlからデータを取ってくるように実装中

--- services/server/src/main.go
+++ services/server/src/main.go
@@ -4,15 +4,50 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+	"nabelog/article"
+	
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func open(path string, count uint) *sql.DB {
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		log.Fatal("open error:", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		time.Sleep(time.Second * 2)
+		count--
+		fmt.Printf("retry... count:%v\n", count)
+		return open(path, count)
+	}
+
+	fmt.Println("db connected!!")
+	return db
+}
+
+func connectDB() *sql.DB {
+	var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
+		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"))
+
+	return open(path, 100)
+}
+
 func main() {
-	log.Println("start server...")
-	r := gin.Default()
-	r.GET("/hello", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
-	log.Fatal(r.Run())
+	db := connectDB()
+	defer db.Close()
+	article.ReadAll(db)
+	// log.Println("start server...")
+	// r := gin.Default()
+	// r.GET("/hello", func(context *gin.Context) {
+	// 	context.JSON(200, gin.H{
+	// 		"message": "Hello World!",
+	// 	})
+	// })
+	// log.Fatal(r.Run())
 }
