13332980a9fb7b2df1e5adec9b71360d9be7e497
ayumu-1212 <ayumuabe1434@gmail.com>
2022-11-18 17:59:44 +0900

    :bookmark: envファイルが使えることを確認

--- services/server/src/main.go
+++ services/server/src/main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	// "github.com/jinzhu/gorm"
 )
 
 func main() {
-	log.Println("start server...")
-	r := gin.Default()
-	r.GET("/hello", func(context *gin.Context) {
+	// db := sqlConnect()
+	// defer db.Close()
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(200, "index.html", gin.H{})
+	})
+
+	database := os.Getenv("MYSQL_DATABASE")
+
+	router.GET("/hello", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"message": "Hello World!",
+			"database": database,
 		})
 	})
-	log.Fatal(r.Run())
+	log.Fatal(router.Run())
 }
+
+// func sqlConnect() (database *gorm.DB) {
+// 	os.Getenv("MYSQL_DATABASE")
+// 	os.Getenv("MYSQL_USER")
+// 	os.Getenv("MYSQL_PASSWORD")
+
+
+// }
