184fd2fbf2ae1cea6bf941dafd339bdc498ea8e5
ayumu-1212 <ayumuabe1434@gmail.com>
2022-11-19 04:22:09 +0900

    :bookmark: gorm実装、dbコンテナから取得とpost実装

--- services/server/src/main.go
+++ services/server/src/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"log"
 	"os"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 )
 
+type Shop struct {
+	gorm.Model
+	Name string
+	Description string
+}
+
 func main() {
-	// db := sqlConnect()
-	// defer db.Close()
+	db := sqlConnect()
+	db.AutoMigrate(&Shop{})
+	defer db.Close()
 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
@@ -19,21 +30,83 @@ func main() {
 		context.HTML(200, "index.html", gin.H{})
 	})
 
-	database := os.Getenv("MYSQL_DATABASE")
-
 	router.GET("/hello", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"message": "Hello World!",
-			"database": database,
 		})
 	})
+
+	router.GET("/shops", func(context *gin.Context) {
+		db := sqlConnect()
+		var shops []Shop
+		db.Order("created_at asc").Find(&shops)
+		defer db.Close()
+		context.JSON(200, gin.H{
+			"message": "get shops",
+			"shops": shops,
+		})
+	})
+
+  router.POST("/shops/new", func(ctx *gin.Context) {
+    db := sqlConnect()
+		fmt.Println(ctx)
+    name := ctx.PostForm("name")
+    description := ctx.PostForm("description")
+    fmt.Println("create user " + name + " with description " + description)
+    db.Create(&Shop{Name: name, Description: description})
+    defer db.Close()
+
+    ctx.Redirect(302, "/")
+  })
+
+  // router.POST("/delete/:id", func(ctx *gin.Context) {
+  //   db := sqlConnect()
+  //   n := ctx.Param("id")
+  //   id, err := strconv.Atoi(n)
+  //   if err != nil {
+  //     panic("id is not a number")
+  //   }
+  //   var user User
+  //   db.First(&user, id)
+  //   db.Delete(&user)
+  //   defer db.Close()
+
+  //   ctx.Redirect(302, "/")
+  // })
 	log.Fatal(router.Run())
 }
 
-// func sqlConnect() (database *gorm.DB) {
-// 	os.Getenv("MYSQL_DATABASE")
-// 	os.Getenv("MYSQL_USER")
-// 	os.Getenv("MYSQL_PASSWORD")
+func sqlConnect() (database *gorm.DB) {
+	DBMS := "mysql"
+	DBNAME := os.Getenv("MYSQL_DATABASE")
+	USER := os.Getenv("MYSQL_USER")
+	PASS := os.Getenv("MYSQL_PASSWORD")
+	PROTOCOL := "tcp(db:3306)"
 
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
 
-// }
+	fmt.Println(CONNECT)
+
+	count := 0
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		for {
+			if err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Print(".")
+			time.Sleep(time.Second)
+			count++
+			if count > 180 {
+				fmt.Println("")
+				fmt.Println("DB接続失敗")
+				panic(err)
+			}
+			db, err = gorm.Open(DBMS, CONNECT)
+		}
+	}
+	fmt.Println("DB接続成功")
+
+	return db
+}
