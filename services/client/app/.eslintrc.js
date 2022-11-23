66de2a6ed67f14cc38e17f6158a28482f111f7be
ayumu-1212 <ayumuabe1434@gmail.com>
2022-11-23 16:48:01 +0900

    🔧 eslint, prettierの設定を変更

--- services/client/app/.eslintrc.js
+++ services/client/app/.eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
-  }
+    'comma-dangle': ['error', 'only-multiline'],
+    'space-before-function-paren': 0,
+    '@typescript-eslint/space-before-function-paren': 'warn',
+  },
 }
