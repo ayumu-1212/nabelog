391ebb7a4200bf2c6aecda9f3eab6e5cf0945edb
ayumu-1212 <ayumuabe1434@gmail.com>
2022-12-03 22:12:37 +0900

    🚨 lintの指摘を修正

--- services/client/app/src/entity/shop.ts
+++ services/client/app/src/entity/shop.ts
@@ -1,10 +1,10 @@
-type Shop = {
-  ID: number,
-  Name: string,
-  Description: string,
-  CreatedAt: string,
-  UpdatedAt: string,
-  DeletedAt: string | null,
+interface Shop {
+  ID: number
+  Name: string
+  Description: string
+  CreatedAt: string
+  UpdatedAt: string
+  DeletedAt: string | null
 }
 
-export default Shop;
+export default Shop
