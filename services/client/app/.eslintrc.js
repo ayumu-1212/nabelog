1b190b2d7bddb0f68a810053ff897b477ea639f9
ayumu-1212 <ayumuabe1434@gmail.com>
2022-12-03 22:31:58 +0900

    🔧 lintにimport文の設定を追加

--- services/client/app/.eslintrc.js
+++ services/client/app/.eslintrc.js
@@ -10,11 +10,74 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint', 'unused-imports', 'import'],
   rules: {
     'comma-dangle': ['error', 'only-multiline'],
     '@typescript-eslint/comma-dangle': 0,
     'space-before-function-paren': 0,
     '@typescript-eslint/space-before-function-paren': 0,
+    '@typescript-eslint/no-unused-vars': 'off', // or "no-unused-vars"
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+    'import/order': [
+      'error',
+      {
+        // グループごとの並び順
+        groups: [
+          'builtin', // 1. fsや path などの node "builtin" のモジュール
+          'external', // 2. npm install したパッケージ
+          'internal', // 3. webpack などでパス設定したモジュール
+          ['parent', 'sibling'], // 4. 親階層と小階層のファイル
+          'object', // object"-imports
+          'type', // 型だけをインポートする type imports
+          'index', // 同階層のファイル
+        ],
+        // グループごとに改行を入れる
+        'newlines-between': 'always', // "never" を指定すると改行なし
+        // FIXME: ちょっとよく分かってない
+        // This defines import types that are not handled by configured pathGroups. This is mostly needed when you want to handle path groups that look like external imports.
+        pathGroupsExcludedImportTypes: ['builtin'],
+        // アルファベット順・大文字小文字を区別しない
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        // パターンマッチしたものをグループにする
+        // "newlines-between": "always" の場合は pathGroups  ごとに空行が入る
+        pathGroups: [
+          // react 関連を external より前にする
+          // "pathGroupsExcludedImportTypes": ["react"], にしてみたが `react`, `react-dom` などが別グループになってしまったので pattern で無理やり同じグループにした
+          {
+            pattern: 'react**',
+            group: 'external',
+            position: 'before',
+          },
+          // `@/entity` の import をグルーピング
+          {
+            pattern: '@/entity/**',
+            group: 'internal',
+            position: 'before',
+          },
+          // `@/pages` の import をグルーピング
+          {
+            pattern: '@/pages/**',
+            group: 'internal',
+            position: 'before',
+          },
+          // `@/components` の import をグルーピング
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+          // CSS module を一番最後に
+          {
+            pattern: './**.module.css',
+            group: 'index',
+            position: 'after',
+          },
+        ],
+      },
+    ],
   },
 }
