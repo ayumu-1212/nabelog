b186012e360802b645b722433cb0c261bd839852
ayumu-1212 <ayumuabe1434@gmail.com>
2022-12-03 16:13:31 +0900

    🎨 HeaderとContainerを導入

--- services/client/app/src/App.tsx
+++ services/client/app/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Container from '@mui/material/Container'
 import ShopList from './pages/ShopList'
 import Default from './pages/Default'
 import ShopDetail from './pages/ShopDetail'
+import ButtonAppBar from './components/organisms/Header'
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Default />} />
-        <Route path="/shops" element={<ShopList />} />
-        <Route path="/shops/:id" element={<ShopDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ButtonAppBar />
+      <Container maxWidth="lg" sx={{ pt: 3 }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Default />} />
+            <Route path="/shops" element={<ShopList />} />
+            <Route path="/shops/:id" element={<ShopDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </>
   )
 }
 
---
+++ services/client/app/src/components/organisms/Header.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+
+export default function ButtonAppBar(): JSX.Element {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Nabelog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
