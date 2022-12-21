6ccd585011a1da196ba337079c4fec2d60782d9e
ayumu-1212 <ayumuabe1434@gmail.com>
2022-12-21 11:13:33 +0900

    ✨ envによって、CORSの許可ドメインを変える

---
+++ services/server/src/config/env.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"os"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func SetEnv() {
+	env := os.Getenv("NABELOG_ENV")
+	if "" == env {
+		env = "development"
+	}
+
+	err := godotenv.Load(".env." + env)
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+}
--- services/server/src/main.go
+++ services/server/src/main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"log"
 	"os"
+	"log"
 	"fmt"
-	"strconv"
 	"time"
 
 	"nabelog.location/model"
+	"nabelog.location/config"
 
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	"github.com/gin-contrib/cors"
 )
 
 func main() {
-	db := sqlConnect()
-	db.AutoMigrate(&model.Shop{}, &model.Influencer{})
-	defer db.Close()
-
+	config.SetEnv()
+	fmt.Println(os.Getenv("CLIENT_URL"))
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*.html")
-
+	if os.Getenv("NABELOG_ENV") == "production" {
+		// 本番環境
+		router.LoadHTMLGlob("../src/templates/*.html")
+	} else {
+		// 開発環境
+		router.LoadHTMLGlob("templates/*.html")
+	}
   router.Use(cors.New(cors.Config{
     AllowOrigins: []string{
-			"http://localhost:3000",
-			// "https://127.0.0.1:3000",
+			os.Getenv("CLIENT_URL"),
     },
     AllowMethods: []string{
 			"POST",
@@ -58,11 +59,18 @@ func main() {
 		})
 	})
 
+	var shop model.Shop
+	shop.ID = 12
+	shop.CreatedAt = time.Now()
+	shop.UpdatedAt = time.Now()
+	shop.DeletedAt = nil
+	shop.Name = "what do you mean"
+	shop.Description = "yes i so yeah"
+
+	shops := [] model.Shop{shop, shop}
+	
+
 	router.GET("/shops", func(context *gin.Context) {
-		db := sqlConnect()
-		var shops []model.Shop
-		db.Order("created_at asc").Find(&shops)
-		defer db.Close()
 		context.JSON(200, gin.H{
 			"message": "get shops",
 			"shops": shops,
@@ -70,26 +78,10 @@ func main() {
 	})
 
   router.POST("/shops", func(context *gin.Context) {
-    db := sqlConnect()
-    name := context.PostForm("name")
-    description := context.PostForm("description")
-    fmt.Println("create shop " + name + " with description " + description)
-    db.Create(&model.Shop{Name: name, Description: description})
-    defer db.Close()
-
     context.Redirect(302, "/")
   })
 
 	router.GET("/shops/:id", func(context *gin.Context) {
-		db := sqlConnect()
-		n := context.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("id is not a number")
-		}
-		var shop model.Shop
-		db.First(&shop, id)
-		defer db.Close()
 		context.JSON(200, gin.H{
 			"message": "get shop",
 			"shop": shop,
@@ -97,22 +89,6 @@ func main() {
 	})
 
 	router.PATCH("/shops/:id", func(context *gin.Context) {
-		db := sqlConnect()
-		n := context.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("id is not a number")
-		}
-		var shop model.Shop
-		db.First(&shop, id)
-
-    name := context.PostForm("name")
-    description := context.PostForm("description")
-		shop.Name = name
-		shop.Description = description
-		db.Save(&shop)
-
-		defer db.Close()
 		context.JSON(200, gin.H{
 			"message": "get shop",
 			"shop": shop,
@@ -121,154 +97,52 @@ func main() {
 
 
   router.DELETE("/shops/:id", func(context *gin.Context) {
-		db := sqlConnect()
-		n := context.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("id is not a number")
-		}
-		var shop model.Shop
-		db.First(&shop, id)
-    db.Delete(&shop)
-    defer db.Close()
-
     context.Redirect(302, "/")
   })
 
+	var influencer model.Influencer
+	influencer.ID = 12
+	influencer.CreatedAt = time.Now()
+	influencer.UpdatedAt = time.Now()
+	influencer.DeletedAt = nil
+	influencer.Name = "chanabe"
+	influencer.Description = "chanabe"
+	influencer.InstagramLink = "https://www.instagram.com/ayumu12.12/"
+	influencer.TwitterLink = "https://twitter.com/chanabe1212"
+	influencer.YoutubeLink = ""
+	influencer.TiktokLink = ""
+	influencer.WebLink = ""
+
+	influencers := [] model.Influencer{influencer, influencer}
+
 	router.GET("/influencers", func(context *gin.Context) {
-		db := sqlConnect()
-		var influencers []model.Influencer
-		db.Order("created_at asc").Find(&influencers)
-		defer db.Close()
 		context.JSON(200, gin.H{
-			"message": "get influencers",
 			"influencers": influencers,
+			"message": "get influencers",
 		})
 	})
 
 	router.POST("/influencers", func(context *gin.Context) {
-    db := sqlConnect()
-    name := context.PostForm("name")
-    description := context.PostForm("description")
-    instagramLink := context.PostForm("instagramLink")
-    twitterLink := context.PostForm("twitterLink")
-    youtubeLink := context.PostForm("youtubeLink")
-    tiktokLink := context.PostForm("tiktokLink")
-    webLink := context.PostForm("webLink")
-    fmt.Println("create influencer " + name + " with description " + description)
-    db.Create(&model.Influencer{
-			Name: name, 
-			Description: description, 
-			InstagramLink: instagramLink, 
-			TwitterLink: twitterLink, 
-			YoutubeLink: youtubeLink, 
-			TiktokLink: tiktokLink, 
-			WebLink: webLink,
-		})
-    defer db.Close()
-
     context.Redirect(302, "/")
   })
 
 	router.GET("/influencers/:id", func(context *gin.Context) {
-		db := sqlConnect()
-		n := context.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("id is not a number")
-		}
-		var influencer model.Influencer
-		db.First(&influencer, id)
-		defer db.Close()
 		context.JSON(200, gin.H{
-			"message": "get influencer",
 			"influencer": influencer,
+			"message": "get influencer",
 		})
 	})
 
 	router.PATCH("/influencers/:id", func(context *gin.Context) {
-		db := sqlConnect()
-		n := context.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("id is not a number")
-		}
-		var influencer model.Influencer
-		db.First(&influencer, id)
-
-    name := context.PostForm("name")
-    description := context.PostForm("description")
-    instagramLink := context.PostForm("instagramLink")
-    twitterLink := context.PostForm("twitterLink")
-    youtubeLink := context.PostForm("youtubeLink")
-    tiktokLink := context.PostForm("tiktokLink")
-    webLink := context.PostForm("webLink")
-
-		influencer.Name = name
-		influencer.Description = description
-		influencer.InstagramLink = instagramLink
-		influencer.TwitterLink = twitterLink
-		influencer.YoutubeLink = youtubeLink
-		influencer.TiktokLink = tiktokLink
-		influencer.WebLink = webLink
-		db.Save(&influencer)
-
-		defer db.Close()
 		context.JSON(200, gin.H{
-			"message": "get influencer",
 			"influencer": influencer,
+			"message": "get influencer",
 		})
 	})
 
   router.DELETE("/influencers/:id", func(context *gin.Context) {
-		db := sqlConnect()
-		n := context.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("id is not a number")
-		}
-		var influencer model.Influencer
-		db.First(&influencer, id)
-    db.Delete(&influencer)
-    defer db.Close()
-
     context.Redirect(302, "/")
   })
 
 	log.Fatal(router.Run())
-}
-
-func sqlConnect() (database *gorm.DB) {
-	DBMS := "mysql"
-	DBNAME := os.Getenv("MYSQL_DATABASE")
-	USER := os.Getenv("MYSQL_USER")
-	PASS := os.Getenv("MYSQL_PASSWORD")
-	PROTOCOL := "tcp(db:3306)"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-
-	fmt.Println(CONNECT)
-
-	count := 0
-	db, err := gorm.Open(DBMS, CONNECT)
-	if err != nil {
-		for {
-			if err == nil {
-				fmt.Println("")
-				break
-			}
-			fmt.Print(".")
-			time.Sleep(time.Second)
-			count++
-			if count > 180 {
-				fmt.Println("")
-				fmt.Println("DB接続失敗")
-				panic(err)
-			}
-			db, err = gorm.Open(DBMS, CONNECT)
-		}
-	}
-	fmt.Println("DB接続成功")
-
-	return db
 }
